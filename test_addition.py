93e2cf3550712173c34fe2b7bfd6c60c98737088
balssam <127256815+balssam@users.noreply.github.com>
2023-03-08 15:53:05 +0100

    thirddd

--- test_addition.py
+++ test_addition.py
@@ -1,33 +1,12 @@
-import unittest
-from io import StringIO
-import sys
-from program import somme_deux_nombres
+def test_somme():
+    a = 2
+    b = 3
+    assert (a + b) == 5
 
-class TestSommeDeuxNombres(unittest.TestCase):
+    a = 0
+    b = 0
+    assert (a + b) == 0
 
-    def test_somme_deux_nombres(self):
-        captured_output = StringIO()
-        sys.stdout = captured_output
-
-        somme_deux_nombres(5, 7)
-        expected_output = "La somme de 5 et 7 est 12\n"
-        self.assertEqual(captured_output.getvalue(), expected_output)
-
-        captured_output = StringIO()
-        sys.stdout = captured_output
-
-        somme_deux_nombres(-3, 8)
-        expected_output = "La somme de -3 et 8 est 5\n"
-        self.assertEqual(captured_output.getvalue(), expected_output)
-
-        captured_output = StringIO()
-        sys.stdout = captured_output
-
-        somme_deux_nombres(0, 0)
-        expected_output = "La somme de 0 et 0 est 0\n"
-        self.assertEqual(captured_output.getvalue(), expected_output)
-
-        sys.stdout = sys.__stdout__
-
-if __name__ == '__main__':
-    unittest.main()
+    a = -5
+    b = 5
+    assert (a + b) == 0
