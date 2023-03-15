ef4151eba9ac349e4780a321e5d782fa81c8e23a
balssam <127256815+balssam@users.noreply.github.com>
2023-03-15 10:20:18 +0100

    test

--- addition.py
+++ addition.py
@@ -1,3 +1,6 @@
+"""
+Ce programme demande à l'utilisateur deux nombres et calcule leur somme.
+"""
 a = int(input("Entrez le premier nombre: "))
 b = int(input("Entrez le deuxième nombre: "))
 somme = a + b
